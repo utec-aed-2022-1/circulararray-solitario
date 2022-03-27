14fd06efa001f2ea37c118b9301fa00a6d410a08
Leonardo <leonardo.vasquez@utec.edu.pe>
2022-03-27 15:04:57 -0500

    feat(CircularArray): Adición de funciones
    Se implementaron las funciones REVERSE, PUSH_FRONT, POP_BACK, INSERT, OPERATOR[]

--- circulararray.h
+++ circulararray.h
@@ -13,16 +13,16 @@ public:
     CircularArray();
     CircularArray(int _capacity);
     virtual ~CircularArray();
-    void push_front(T data);
+    void push_front(T data); //CHECK
     void push_back(T data);
-    void insert(T data, int pos);
-    T pop_front();
-    T pop_back();
-    bool is_full();
+    void insert(T data, int position); //CHECK
+    T pop_front(); 
+    T pop_back();  //CHECK
+    bool is_full();  
     bool is_empty();
     int size();
     void clear();
-    T &operator[](int);
+    T &operator[](int); //CHECK
     void sort();
     bool is_sorted();
     void reverse();
@@ -79,6 +79,26 @@ int CircularArray<T>::size() {
     return back - front + 1;
 }
 
+template <class T>
+
+
+template <class T> // PUSH FRONT
+void CircularArray<T>::push_front(T data){
+  if(is_empty()){
+    front = back = 0;
+    array[0] = data;
+  }
+  else if (is_full()) {
+    front = prev(front);
+    back = prev(back);
+    array[front] = data;
+  }
+  else {
+    front = prev(front);
+    array[front] = data;
+  }
+};
+
 template <class T>//PUSH BACK
 void CircularArray<T>::push_back(T data) {  
   if(is_empty())
@@ -100,6 +120,22 @@ T CircularArray<T>::pop_front() {
     back= prev(back);
     return aux ;
 }
+
+template <class T> //POP BACK
+T CircularArray<T>::pop_back() {
+  T aux;
+  if (is_empty()){
+    aux = -1;
+    throw("No puede extraerse valores. Array vacío.");
+  }
+  else {
+    aux = array[back];
+    array[back] = 0;
+    back = prev(back);
+  }
+  return aux;
+};
+
 template <class T>//IS _FULL    
 bool CircularArray<T>::is_full() {
   return next(back) == front;
@@ -109,6 +145,35 @@ bool CircularArray<T>::is_empty() {
   return (front == -1 && back == -1);
 }
 
+template <class T> //OPERATOR[]
+T &CircularArray<T>::operator[](int i){
+  return array[i];
+};
+
+template <class T> //INSERT
+void CircularArray<T>::insert(T data, int position){
+  if (position < data){
+    array[position] = data;
+  }
+  else {
+    throw("Posición fuera del rango.");
+  }
+};
+
+template <class T> //REVERSE
+T* CircularArray<T>::reverse(){
+  T* new_array = new T[capacity];
+  T front_temp = front;
+  T back_temp = back;
+
+  for (int i = 0; i < capacity; i++){
+      new_array[front_temp] = array[back_temp];
+      front_temp = next(front_temp);
+      back_temp = prev(back_temp);
+  };
+
+  return new_array;
+};
 
 template <class T>
 string CircularArray<T>::to_string(string sep)
