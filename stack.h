04172b2ecd7ea1ea17f6841e4862dbf4a990d3c7
HaroldCV <70811385+HaroldCV@users.noreply.github.com>
2022-04-01 17:32:47 -0500

    Update stack.h

--- stack.h
+++ stack.h
@@ -3,6 +3,20 @@
 template <typename T>
 class StackArray : public CircularArray<T> { 
   public:
-  Stack();
+  StackArray();
+  StackArray(int capacity);
 
+  void push(T item);
+  T pop();
+  T top();
 };
+
+template <typename T>
+void StackArray<T>::push(T item) {
+  StackArray<T>::push_back(item);
+}
+
+template <typename T>
+T StackArray<T>::pop() {
+  return StackArray<T>::pop_back(); 
+}
